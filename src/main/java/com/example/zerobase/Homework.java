34b677ed045546e23cc02570f77373c44d22171a
CHOI <jack96318@hanmail.net>
2022-01-10 23:10:34 +0900

    homework 8th -1

--- src/main/java/com/example/zerobase/Homework.java
+++ src/main/java/com/example/zerobase/Homework.java
@@ -5,6 +5,7 @@ import com.example.zerobase.domain.ZerobaseCourseRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,12 +17,33 @@ public class Homework {
 
     public Optional<ZerobaseCourse> getZerobaseCourse(Long id) {
         // TODO: id 가 일치하며, hidden = false 인 강의만 조회되어야 함
-        return null;
+
+        ZerobaseCourse zerobaseCourse = repository.findById(id);
+
+        if (zerobaseCourse == null || zerobaseCourse.isHidden()){
+            return Optional.empty();
+        }
+
+        return Optional.of(zerobaseCourse);
     }
 
     public List<ZerobaseCourse> getZerobaseCourse(String status) {
         // TODO: status가 일치하고, hidden = false 인 강의들이 조회되어야 함
-        return null;
+
+        List<ZerobaseCourse> listTest = repository.findAll();
+        List<ZerobaseCourse> list = null;
+
+        for (int i = 0; i < listTest.size(); i++){
+            if(listTest.get(i).getStatus() == status && !listTest.get(i).isHidden()){
+                System.out.println("리스트 값 : " + listTest.get(i).getStatus());
+                System.out.println("입력값 : " + status);
+                System.out.println(listTest.get(i));
+//                list.addAll(list.size(), listTest.get(i));
+                list.add(0, listTest.get(i));
+            }
+        }
+
+        return list;
     }
 
     public List<ZerobaseCourse> getOpenZerobaseCourse(LocalDate targetDt) {
